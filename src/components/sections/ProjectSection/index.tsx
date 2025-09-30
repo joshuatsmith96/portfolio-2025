95296e5c4fbffc9829024200cf6ffe96fc1fcdc7
joshuatsmith96 <joshuatsmith96@gmail.com>
2025-09-30 17:54:29 -0400

    Added more ESLint rules

--- src/components/sections/ProjectSection/index.tsx
+++ src/components/sections/ProjectSection/index.tsx
@@ -4,36 +4,46 @@ import { ProjectTile } from '../../blocks/ProjectTile';
 import { Tiles } from '../../../data/TileData';
 
 export const ProjectSection = () => {
-
-    return (
-        <Section sx={{ paddingBottom: '150px' }} id="projects">
-            <Divider sx={{
-                '&::before, &::after': {
-                    borderColor: '#c4c4c4ff',
-                },
-            }}>
-                <Typography color="#c4c4c4ff" px={{ xs: 0, sm: 5 }} fontSize={{ xs: '16px' }}>Some of my latest work</Typography>
-            </Divider>
-            {/* Rest of content here */}
-            <Stack gap={10} sx={{
-                alignItems: { sm: 'center' },
-                justifyContent: { sm: 'center' },
-            }}>
-                {Tiles.map((tile, index) => (
-                    <ProjectTile
-                        key={index}
-                        imgSrc={tile.imgSrc}
-                        imgAlt={tile.imgAlt}
-                        title={tile.title}
-                        skills={tile.skills}
-                        description={tile.description}
-                        githubLink={tile.githubLink}
-                        figmaLink={tile.figmaLink}
-                        demoLink={tile.demoLink}
-                        reverse={tile.reverse ? true : false}
-                    />
-                ))}
-            </Stack>
-        </Section >
-    );
+  return (
+    <Section sx={{ paddingBottom: '150px' }} id="projects">
+      <Divider
+        sx={{
+          '&::before, &::after': {
+            borderColor: '#c4c4c4ff',
+          },
+        }}
+      >
+        <Typography
+          color="#c4c4c4ff"
+          px={{ xs: 0, sm: 5 }}
+          fontSize={{ xs: '16px' }}
+        >
+          Some of my latest work
+        </Typography>
+      </Divider>
+      {/* Rest of content here */}
+      <Stack
+        gap={10}
+        sx={{
+          alignItems: { sm: 'center' },
+          justifyContent: { sm: 'center' },
+        }}
+      >
+        {Tiles.map((tile, index) => (
+          <ProjectTile
+            key={index}
+            imgSrc={tile.imgSrc}
+            imgAlt={tile.imgAlt}
+            title={tile.title}
+            skills={tile.skills}
+            description={tile.description}
+            githubLink={tile.githubLink}
+            figmaLink={tile.figmaLink}
+            demoLink={tile.demoLink}
+            reverse={tile.reverse ? true : false}
+          />
+        ))}
+      </Stack>
+    </Section>
+  );
 };
--- src/data/TileData.ts
+++ src/data/TileData.ts
@@ -1,49 +1,41 @@
 import type { ProjectTileType } from '../types/ProjectTypes';
 import TestImage from '../assets/pattern1.png';
 import { React, TypeScript, MUI } from './SkillAttributeData';
+import PortfolioSite from '../assets/project-images/portfolio-image.png';
 
 export const Tiles: ProjectTileType[] = [
-    {
-        imgSrc: TestImage,
-        imgAlt: 'Test alt text',
-        title: 'Test Project',
-        skills: [
-            React,
-            TypeScript,
-            MUI
-        ],
-        description: 'A quick explanation and description of the project, explaining what it does, and what my inspiration was. I don’t need to do too much detail.',
-        githubLink: 'google.com',
-        figmaLink: 'test',
-        demoLink: 'test'
-    },
-    {
-        imgSrc: TestImage,
-        imgAlt: 'Test alt text',
-        title: 'Test Project',
-        skills: [
-            React,
-            TypeScript,
-            MUI
-        ],
-        description: 'A quick explanation and description of the project, explaining what it does, and what my inspiration was. I don’t need to do too much detail.',
-        githubLink: 'google.com',
-        figmaLink: 'test',
-        demoLink: 'test',
-        reverse: true
-    },
-    {
-        imgSrc: TestImage,
-        imgAlt: 'Test alt text',
-        title: 'Test Project',
-        skills: [
-            React,
-            TypeScript,
-            MUI
-        ],
-        description: 'A quick explanation and description of the project, explaining what it does, and what my inspiration was. I don’t need to do too much detail.',
-        githubLink: 'google.com',
-        figmaLink: 'test',
-        demoLink: 'test'
-    }
+  {
+    imgSrc: PortfolioSite,
+    imgAlt: 'Test alt text',
+    title: 'Test Project',
+    skills: [React, TypeScript, MUI],
+    description:
+      'A quick explanation and description of the project, explaining what it does, and what my inspiration was. I don’t need to do too much detail.',
+    githubLink: 'google.com',
+    figmaLink: 'test',
+    demoLink: 'test',
+  },
+  {
+    imgSrc: TestImage,
+    imgAlt: 'Test alt text',
+    title: 'Test Project',
+    skills: [React, TypeScript, MUI],
+    description:
+      'A quick explanation and description of the project, explaining what it does, and what my inspiration was. I don’t need to do too much detail.',
+    githubLink: 'google.com',
+    figmaLink: 'test',
+    demoLink: 'test',
+    reverse: true,
+  },
+  {
+    imgSrc: TestImage,
+    imgAlt: 'Test alt text',
+    title: 'Test Project',
+    skills: [React, TypeScript, MUI],
+    description:
+      'A quick explanation and description of the project, explaining what it does, and what my inspiration was. I don’t need to do too much detail.',
+    githubLink: 'google.com',
+    figmaLink: 'test',
+    demoLink: 'test',
+  },
 ];
