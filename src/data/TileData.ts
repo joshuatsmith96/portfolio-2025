5290f7408f6fdb72e6c669d9917afbb67f9c46c8
joshuatsmith96 <joshuatsmith96@gmail.com>
2025-10-08 09:18:51 -0400

    Fixed incorrect link on on of the projects

--- src/data/TileData.ts
+++ src/data/TileData.ts
@@ -29,11 +29,11 @@ export const Tiles: ProjectTileType[] = [
     title: 'SoleStyle - Ecommerce',
     skills: [React, TypeScript, Tailwind],
     description:
-      'This site is a demonstration of my front-end skills. I built it as a way to explore creating a front-end eCommerce platform. You’ll notice there’s no backend, and that’s intentional. The focus is entirely on the frontend, while still giving the appearance of dynamic backend responses.',
+      'A UI for an e-commerce store. This site is a demonstration of my front-end skills. I built it as a way to explore creating a front-end eCommerce platform. You’ll notice there’s no backend, and that’s intentional. The focus is entirely on the frontend, while still giving the appearance of dynamic backend responses.',
     githubLink: 'https://github.com/joshuatsmith96/portfolio-2025',
     demoLink: 'https://solestyle.joshuasportfolio.org',
     figmaLink:
-      'https://www.figma.com/design/bVs4UssoLMTQxPlMAeKtkC/Untitled?node-id=0-1&p=f&t=5aMPw4xXeYHsYE6I-0',
+      'https://figma.com/design/MzhwStaWgCcDWfJmPZUGoL/Shoe-Commerce-Store?t=pc1KMvG9NGVzS82l-0',
     reverse: true,
   },
   {
