0e92669b6be24ab31ee38482d878d7f1231455b2
joshuatsmith96 <joshuatsmith96@gmail.com>
2026-01-05 12:20:59 -0500

    Adjusted some experience information

--- src/data/ExperienceData.ts
+++ src/data/ExperienceData.ts
@@ -1,27 +1,26 @@
 export const ExperienceData = [
-    {
-        companyName: 'Midwest Veterinary Supply, Inc.',
-        title: 'React Web Developer',
-        datesWorked: 'March 2024 - Present',
-        workType: 'Remote',
-        duties: [
-            'The primary front-end React / Next.js developer.',
-            'Creating APIs that connect directly to our AS400 database.',
-            'Integrating API data into the front-end.',
-            'Completing and creating tickets in Jira.',
-            'Create mock-ups using Figma for all new pages.'
-        ]
-    },
-    {
-        companyName: 'Daily Printing, Inc.',
-        title: 'Storefront Administrator & Variable Data Programmer',
-        datesWorked: 'March 2024 - Present',
-        workType: 'Remote',
-        duties: [
-            'Creating storefronts for new customers using HTML, CSS and JavaScript',
-            'Creating custom API integration onto customer storefronts',
-            'Variable data programming using EPS SmartCanvas program.',
-            'Administrator for over 30 customer storefronts.'
-        ]
-    }
+  {
+    companyName: 'Midwest Veterinary Supply, Inc.',
+    title: 'React Web Developer',
+    datesWorked: 'March 2024 - Present',
+    workType: 'Remote',
+    duties: [
+      'The primary front-end React / Next.js developer.',
+      'Creating APIs that connect directly to our AS400 database.',
+      'Integrating API data into the front-end.',
+      'Completing and creating tickets in Jira.',
+      'Create mock-ups using Figma for all new pages.',
+    ],
+  },
+  {
+    companyName: 'Daily Printing, Inc.',
+    title: 'Storefront Administrator & Variable Data Programmer',
+    datesWorked: 'May 2022 - Present',
+    workType: 'Remote',
+    duties: [
+      'Creating storefronts for new customers using HTML, CSS and JavaScript',
+      'Variable data programming using EPS SmartCanvas program.',
+      'Administrator for over 30 customer storefronts.',
+    ],
+  },
 ];
